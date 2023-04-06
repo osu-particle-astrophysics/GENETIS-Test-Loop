1844549f9e2def681d4caa366f6ce5143462d705
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-04-06 16:08:32 -0400

    Update estimate_time_AREA.py

--- AREA/estimate_time_AREA.py
+++ AREA/estimate_time_AREA.py
@@ -1,7 +1,8 @@
 # estimate the time it will take the test loop to run
 
 runCount=0
-generations=0
+secpergen = 5
+generations = 50
 
 for a in range(0,101,10): #RC
     for b in range(0,101,10): #RM
@@ -11,11 +12,9 @@ for a in range(0,101,10): #RC
             if p==100:
               for h in range(0,11,1):
                 runCount=runCount+1
-                for g in range(1,50,1):
-                  generations = generations+1
 
 batches = runCount/250
-seconds = generations*5
+seconds = generations*secpergen*batches
 minutes = int(seconds/60)
 r_sec = seconds - (minutes*60)
 hours = int(minutes/60)
