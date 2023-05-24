74a7643a83adef8171fae6ad21bf1582b8d6eeb4
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-24 19:54:14 -0400

    Update data_write.py
    Updated to take in and print out data for any design.

--- data_write.py
+++ data_write.py
@@ -22,21 +22,24 @@ opperator = []
 fitness = []
 error = [] 
 chi = []
-radius = [] ## temp array
-radius1 = []
-radius2 = []
-length = [] ## temp array
-length1 = []
-length2 = []
-Q = []      ## temp array
-Q1 = []
-Q2 = []
-L =  []     ## temp array
-L1 = []
-L2 = []
+
+# Find DNA vector parameters based on design
+if (g.Design == "ARA"):
+    sections = 2
+    genes = 4
+  
+else if (g.Design == "AREA"):
+    sections = 2
+    genes = 14
+
+else if (g.Design == "PUEO"):
+    sections = 1
+    genes = 7
+  
+# Define list to hold dna
+dna = [[[0]*genes for i in range(sections)] for j in range(g.NPop+1)]
 
 ## read in parent file for seed, individuals, parents, and opperators
-
 with open("parents.csv") as f1:
     csv_read = csv.reader(f1, delimiter = ',')
     for i, row in enumerate(csv_read):
@@ -49,9 +52,7 @@ with open("parents.csv") as f1:
             opperator.append(row[3].rjust(13, ' '))
 f1.close()
 
-
 ## Read in Chi score
-
 with open('chiScores.csv') as f2:
     csv_read = csv.reader(f2, delimiter = ',')
     for i, row in enumerate(csv_read):
@@ -59,9 +60,7 @@ with open('chiScores.csv') as f2:
             chi.append(str(round(float(row[0]), 8)).rjust(12, ' '))
 f2.close()
 
-
 ## Read in fitness score and error
-
 with open('fitnessScores.csv') as f3:
     csv_read = csv.reader(f3, delimiter = ',')
     for i, row in enumerate(csv_read):
@@ -69,46 +68,43 @@ with open('fitnessScores.csv') as f3:
             fitness.append(str(round(float(row[0]), 8)).rjust(10, ' '))
             error.append(str(round(float(row[1]), 8)).rjust(10, ' '))
 f3.close()
-    
 
 ## Read in DNA
-
 with open('generationDNA.csv') as f4:
     csv_read = csv.reader(f4, delimiter = ',')
     for i, row in enumerate(csv_read):
-        if i > 8:
-            radius.append(str(round(float(row[0]), 8)).rjust(10, ' '))
-            length.append(str(round(float(row[1]), 8)).rjust(12, ' '))
-            Q.append(str(round(float(row[2]), 8)).rjust(11, ' '))
-            L.append(str(round(float(row[3]), 8)).rjust(11, ' '))
-f4.close()
-
-
-## Re-sort DNA into sides
-
-for i in range(0, len(radius), 2):
-    radius1.append(radius[i])
-    radius2.append(radius[i+1])
-    length1.append(length[i])
-    length2.append(length[i+1])
-    Q1.append(Q[i])
-    Q2.append(Q[i+1])
-    L1.append(L[i])
-    L2.append(L[i+1])
-
+        if( i > 8):
+            individual = 0
+            if( (i-8)%sections == 0 ):
+                j=0
+            if( (i-8)%sections != 0 ):
+                j=(i-8)%sections
+            for k in range(genes):
+                observed[individual][j][k] = float(row[k])
+            if ( j == (i-8)%sections):
+                individual = individual + 1
 
 ## Write Data into file
-
 with open('generationData.csv', "w") as f5:
     # Write out the header
     f5.write("Generation Data for generation " +g.Gen+ '\n')
     f5.write(seed+ '\n')
     f5.write('\n')
-    f5.write("Individual, Chi, Fitness, Error, Parent 1, Parent 2, Opperator, Radius 1, length 1, Quadratic 1, Linnear 1, Radius 2, Length 2, Quadratic 2, Linnear 2 \n")
+    f5.write("Individual, Chi, Fitness, Error, Parent 1, Parent 2, Opperator,")
+    for x in range(sections*genes):
+        f5.write(" Gene " +str(x)+ ",")
     
     # Write data
     for i in range(len(individual)):
-        f5.write( individual[i] +', '+ chi[i] +', '+ fitness[i] +', '+ error[i] +', '+ parent1[i] +', '+ parent2[i] +', '+ opperator[i] +', '+ \
-                  radius1[i] +', '+ length1[i] +', '+ Q1[i] +', '+ L1[i] +', '+ radius2[i] +', '+ length2[i] +', '+ Q2[i] +', '+ L2[i] +'\n')
-
+        f5.write(str(individual[i]))
+        f5.write(", " + str(chi[i]) )
+        f5.write(", " +str(fitness[i]))
+        f5.write(", " +str(error[i]))
+        f5.write(", " +str(parent1[i]))
+        f5.write(", " +str(parent2[i]))
+        f5.write(", " +str(opperator[i]))
+        for j in range(sections):
+            for i in range(genes):
+                f5.write(", " +str(dna[i][j][k]))
+        f5.write( "\n")
 f5.close()
