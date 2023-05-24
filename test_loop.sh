c972e02185a9d5ce5fdeadcd19b464af7ba55a3f
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-23 22:46:42 -0400

    Update test_loop.sh

--- test_loop.sh
+++ test_loop.sh
@@ -4,86 +4,61 @@
 ## created on 10/23/2020
 ## Created for: GENETIS Research group at Ohio State University
 
+# Compile the GA in its directory
+g++ -std=c++11 GA/SourceFiles/GA.cpp GA/SourceFiles/GA.exe
 
-## Algorithm
+# Set Constants
+Population=100
+Generations=50
+Design="ARA"
 
-# run start for bicone GA
-# run fitness score function
-# run plotting function?
-# run cont for bicone GA
-# repeat for step 2-4 until desired amount of generations is complete
-
-g++ -std=c++11 GA.cpp
-
-pop=100
-arguement=1
-generations=50
-repro=3
-cross=36
-roul_no=2
-tour_no=2
-rank_no=6
-zero=0
-elite=0
-M_rate=5
-sigma=1
-
+# Initialize job submission variables
 Count=0
 start=0
 
-for a in {2..2..2} # Roulette
+# Loop over variables: define them in their ranges in their loops
+for Rank in {2..2..2} 
 do
-    for b in {2..2..2} # Tournament
+    for Roulette in {2..2..2} 
     do
-	for c in {6..6..2} # Rank
-	do
-	    r=$(( $a + $b + $c ))
-	    if [ $r -eq 10 ]
-	    then  
-		for d in {0..12..4} # Reproduction
+		for Tournament in {6..6..2} 
 		do
-		    for e in {88..100..4} # Crossover Make sure Reprodcution + Crossover < pop
-		    do
-			de=$(( $d +$e ))
-			if [ $de -le $pop ]
-			then
-			    for f in {15..25..5} # M_rate
-			    do
-				if [ $f -ne 0 ]
-				then
-				    sigma=7
-				fi
-				if [ $f -eq 0 ] 
-				then
-				    sigma=3
-				fi
-				for (( g=3; g <= $sigma; g++ )) # sigma
+	    	Selection=$(( $Rank + $Roulette + $Tournament ))
+	    	if [ $Selection -eq $Population ]
+	    	then  
+				for Reproduction in {0..12..4} 
 				do
-				    for h in {1..10..1} # test count
-				    do
-					sbatch test_run.sh ${pop} ${generations} ${d} ${e} ${f} ${g} ${a} ${b} ${c} ${elite} ${h}
-					Count=$((Count+1))
-					if [ $Count -ge 250 ]
-					then
-					    echo batch submitted
-					    nfiles=$(ls Plots/ | wc -l)
-					    while [[ $(((nfiles)%500)) -ne 0 || $nfiles -eq $start ]] 
-					    do
-						nfiles=$(ls Plots/ | wc -l)
-						echo $nfiles
-						sleep 10
-					    done
-					    start=$nfiles
-					    Count=0
-					fi
-				    done
-				done
-			    done
-			fi
-		   done
-	      done
-	   fi
-	done
+		    		for Crossover in {88..100..4} 
+		    		do
+						for MutationRate in {15..25..5}
+						do
+							for Sigma in {1..1..1} 
+							do
+				    			for Test in {1..10..1} 
+				   				do
+									# Submit run
+									sbatch test_run.sh ${Design} ${Generations} ${Population} ${Rank} ${Roulette} ${Tournament} ${Reproduction} ${Crossover} ${MutationRate} ${Sigma} ${Test}
+									Count=$((Count+1))
+									if [ $Count -ge 250 ]
+									then
+					    				echo batch submitted
+					    				nfiles=$(ls Plots/ | wc -l)
+					    				while [[ $(((nfiles)%500)) -ne 0 || $nfiles -eq $start ]] 
+					   					do
+										nfiles=$(ls Plots/ | wc -l)
+										echo $nfiles
+										sleep 10
+					    				done
+					    				start=$nfiles
+					    				Count=0
+									fi
+				    			done
+							done
+			    		done
+		   			done
+	      		done
+	   		fi
+		done
     done
 done
 
