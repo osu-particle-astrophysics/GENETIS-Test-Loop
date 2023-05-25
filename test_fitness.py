ba636ac60186f78a06efcd3fb700474bf164bdcb
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-25 14:00:34 -0400

    Update test_fitness.py

--- test_fitness.py
+++ test_fitness.py
@@ -7,7 +7,7 @@ import math
 
 # Functions
 
-def readData(sections, genes, observed):
+def read_data(sections, genes, observed):
     # Read in data from generationDNA.csv and put it into the observed list
     with open('generationDNA.csv') as dna:
         csv_read = csv.reader(dna, delimiter = ',')
@@ -27,7 +27,7 @@ def readData(sections, genes, observed):
                     individual = individual + 1
     dna.close()
             
-def chiSquared(target, observed, sections, genes, chi2):
+def solve_chi_squared(target, observed, sections, genes, chi2):
     # Solve for Chi-squared scores
     for i in range(0, g.NPop):
         tempChi2 = 0
@@ -36,20 +36,21 @@ def chiSquared(target, observed, sections, genes, chi2):
                 tempChi2 = tempChi2 + abs(((observed[i][j][k]-target[j][k])**2)/target[j][k])
         chi2.append(tempChi2)
   
-def calcError(fitness, error):
+def calc_error(fitness, error):
+    # Calculate simulated error for the test
     for i in range(0, len(fitness)):
         if (fitness[i] == 0):
             error.append(0.0)
         else:
-            error.append(0.1)
+            error.append(0.5)
             tempFit = fitness[i]
-            tempFit = random.gauss(fitness[i], 0.0)
+            tempFit = random.gauss(fitness[i], 0.5)
             while(tempFit < 0):
-                tempFit = random.gauss(fitness[i], 0.0)
+                tempFit = random.gauss(fitness[i], 0.5)
             fitness[i] = tempFit
         
-def writeFitness(fitness, error):
-    # Write csv's with data for the generation
+def write_fitness(fitness, error):
+    # Write fintessScore csv with data for the generation
     with open('fitnessScores.csv', "r") as fs:
         lines = fs.readlines()
     fs.close()
@@ -64,7 +65,8 @@ def writeFitness(fitness, error):
         f2.writelines(lines2)
     f2.close
     
-def writeChi(chi2):
+def write_chi(chi2):
+    # Write chiScore csv with data for the generation
     with open('chiScores.csv', "r") as fc:
         lines = fc.readlines()
     fc.close()
@@ -78,36 +80,38 @@ def writeChi(chi2):
                 lines3.append(str(chi2[x-2]) + '\n')
         f2.writelines(lines3)
     f2.close
-
+######################################################
+# MAIN CODE     
+    
 # Parse arguments
 parser = argparse.ArgumentParser();
-parser.add_argument("Design", type=str)
-parser.add_argument("Gen", type=int)
-parser.add_argument("NPop", type=int)
+parser.add_argument("design", type=str)
+parser.add_argument("generation", type=int)
+parser.add_argument("population", type=int)
 g=parser.parse_args()
 
 # Define target gain pattern based on design
-if (g.Design == "ARA"):
+if (g.design == "ARA"):
     target = [[0.968807,14.1263,-0.053024,0.8256], [4.83102,10.4062,0.027465,-0.396757]]
     sections = 2
     genes = 4
   
-elif (g.Design == "AREA"):
+elif (g.design == "AREA"):
     target = [[2.82456,-0.029512,0.0671632,-0.0681442,0.144533,-0.129438,0.00934594,-0.0111579,-0.143811,0.0307403,0.0279309,0.0676385,0.0305001,-2.22847],
 [-1.46998,2.94396,3.07804,-3.72175,3.40754,-0.529288,4.0295,1.26466,3.03942,-4.53036,-3.74864,-0.543697,1.08282,-3.25018]]
     sections = 2
     genes = 14
 
-elif (g.Design == "PUEO"):
+elif (g.design == "PUEO"):
     target = [[33.2056,38.6897,23.6239,14.6074,30.1627,32.4097,1.70987]]
     sections = 1
     genes = 7
   
 # Define list to hold each antennas observed gain pattern
-observed = [[[0]*genes for i in range(sections)] for j in range(g.NPop+1)]
+observed = [[[0]*genes for i in range(sections)] for j in range(g.population+1)]
 
 # Populate the observed list from csv
-readData(sections, genes, observed)
+read_data(sections, genes, observed)
 
 # Define lists to store scores
 fitness = []
@@ -115,19 +119,17 @@ error = []
 chi2 = []
 
 # Calculate ChiSquared Scores
-chiSquared(target, observed, sections, genes, chi2)
+solve_chi_squared(target, observed, sections, genes, chi2)
 
 # Translate Chi-Squared into fitness score
 fitness = [(1/(x+1)) for x in chi2]
 
 # Introduce error to fitness score
-calcError(fitness, error)
+calc_error(fitness, error)
 
 # Write csv's with data for the generation
-writeFitness(fitness, error)            
-writeChi(chi2)
+write_fitness(fitness, error)            
+write_chi(chi2)
 
 print("Max fitness: " + str(max(fitness)))
 print("Min Chi-squared: " + str(min(chi2)))
-
-# END
