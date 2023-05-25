66732c197dbb60fb400231d1357457e7c682284f
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-24 20:23:27 -0400

    Update test_run.sh

--- test_run.sh
+++ test_run.sh
@@ -61,16 +61,22 @@ do
     fi
 
     #Call script to calculate test loop fitness
-    python test_fitness.py $Design $g $population 
+    python test_fitness.py $Design $g $Population 
     
     #Check to see if there are duplicate antennas
-    python fitness_check.py $g
+    python fitness_check.py $Design $g
     
     #Combine all datafiles into one file
-    python data_write.py ${Design} $g
+    python data_write.py $Design $g
     
-    # Move Combined file to permanent directory
-    mv generationData.csv /users/PAS0654/ryantdebolt/test_loop_build_directory/Run/${Rank}'_'${Roulette}'_'${Tournament}'_'${Reproduction}'_'${Crossover}'_'${MutationRate}'_'${Sigma}'_'${Test}'_'${g}_generationData.csv
+    # Copy Combined file to permanent directory
+    cp generationData.csv /users/PAS0654/ryantdebolt/test_loop_build_directory/Run/${Rank}'_'${Roulette}'_'${Tournament}'_'${Reproduction}'_'${Crossover}'_'${MutationRate}'_'${Sigma}'_'${Test}'_'${g}_generationData.csv
+    
+    # Make copies of fitnessScores, generationDNA, and generationData to be used later
+    cp generationData.csv ${g}_generationData.csv
+    cp fitnessScores.csv ${g}_fitnessScores.csv
+    cp generationDNA.csv ${g}_generationDNA.csv
+    
     
     #Show Status
     echo waiting...
@@ -81,7 +87,7 @@ done
 
 #Call plotting scripts
 #Save plots in plot directory with unique names
-python test_plotter.py $population $Generations 
+python test_plotter.py $Population $Generations 
 
 #move plot to the permanent directory
 mv fitness.png /users/PAS0654/ryantdebolt/test_loop_build_directory/Plots/${Rank}'_'${Roulette}'_'${Tournament}'_'${Reproduction}'_'${Crossover}'_'${MutationRate}'_'${Sigma}'_'${Test}_fitness.png
