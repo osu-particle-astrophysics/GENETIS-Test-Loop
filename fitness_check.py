79c3afe9e4c84acef51f64b23a635a52cffc42f2
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-24 22:03:41 -0400

    Update fitness_check.py
    debugging

--- fitness_check.py
+++ fitness_check.py
@@ -4,6 +4,35 @@ import math
 import argparse
 import numpy as np
 
+# Functions
+
+def readFitness(fitness, error, filename):
+    # Read in fitness scores
+    with open(filename) as f1:
+        txt_read = csv.reader(f1, delimiter = ',')
+        for i, row in enumerate(txt_read):
+            if i>1:
+                fitness.append(float(row[0]))
+                error.append(float(row[1]))
+    f1.close()
+
+def readData(sections, genes, dna, filename):
+    # Read in data from generationDNA.csv and put it into the observed list
+    with open(filename) as data:
+        csv_read = csv.reader(data, delimiter = ',')
+        individual = 0
+        for i, row in enumerate(csv_read):
+            if( i > 8):
+                if( (i-9)%sections == 0 ):
+                    j=0
+                if( (i-9)%sections != 0 ):
+                    j=(i-9)%sections
+                for k in range(genes):
+                    dna[individual][j][k] = float(row[k])
+                if ( j == (i-9)%sections and (i-9)%sections != 0):
+                    individual = individual + 1
+
+
 # Read in arguments
 parser = argparse.ArgumentParser();
 parser.add_argument("Design", type=str)
@@ -53,7 +82,7 @@ readData(sections, genes, previous_dna, filename)
 matches = 0
 for i in range(0, len(current_fitness)):
     for j in range(0, len(previous_fitness)):
-        if(current_dna[i] = previous_dna[j]):
+        if(current_dna[i] == previous_dna[j]):
            matches = matches + 1
            current_weight = 1.0/(current_error[i]**2.0)
            previous_weight = 1.0/(previous_error[j]**2.0)
@@ -75,34 +104,3 @@ with open('fitnessScores.csv', 'w') as f6:
             lines2.append(str(current_fitness[x-2]) + "," +str(current_error[x-2]) + '\n')
     f6.writelines(lines2)
 f6.close()
-
-
-#####################################################################################
-
-# Functions
-
-def readFitness(fitness, error, filename):
-    # Read in fitness scores
-    with open(filename) as f1:
-        txt_read = csv.reader(f1, delimiter = ',')
-        for i, row in enumerate(txt_read):
-            if i>1:
-                fitness.append(float(row[0]))
-                error.append(float(row[1]))
-    f1.close()
-
-def readData(sections, genes, dna, filename):
-    # Read in data from generationDNA.csv and put it into the observed list
-    with open(filename) as data:
-        csv_read = csv.reader(data, delimiter = ',')
-        individual = 0
-        for i, row in enumerate(csv_read):
-            if( i > 8):
-                if( (i-9)%sections == 0 ):
-                    j=0
-                if( (i-9)%sections != 0 ):
-                    j=(i-9)%sections
-                for k in range(genes):
-                    dna[individual][j][k] = float(row[k])
-                if ( j == (i-9)%sections and (i-9)%sections != 0):
-                    individual = individual + 1
