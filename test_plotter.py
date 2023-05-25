2c4f76c7480947b119d4125351daca966220d398
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-25 14:14:40 -0400

    Update test_plotter.py
    Fixing convention, adding in the legend to plots.

--- test_plotter.py
+++ test_plotter.py
@@ -9,9 +9,9 @@ from statistics import mean
 
 # Read in arguments
 parser = argparse.ArgumentParser();
-parser.add_argument("Design", type=str)
-parser.add_argument("Gen", type=int)
-parser.add_argument("Npop", type=int)
+parser.add_argument("design", type=str)
+parser.add_argument("generation", type=int)
+parser.add_argument("population", type=int)
 g = parser.parse_args()
 
 # Initialize storage vectors
@@ -26,7 +26,7 @@ ave_chi = []
 
 # Plot each individuals fitness for each generation
 plt.figure(figsize=(16,9))
-for z in range(0, g.Gen+1):
+for z in range(0, g.generation+1):
     
     # Read in all scores for current gen
     with open(str(z)+"_generationData.csv") as f:
@@ -40,14 +40,14 @@ for z in range(0, g.Gen+1):
     ave_fitness.append(mean(fitness_scores))
     
     #populate generation vector for plotting
-    generations = [z for f in range(0, g.Npop)]
+    generations = [z for f in range(0, g.population)]
     
     # begin plotting
-    plt.title(g.Design + 'Fitness Evolution test')
+    plt.title(g.design + ' Fitness Evolution test')
     plt.plot(generations, fitness_scores, '.', color = 'black', markersize = '5.0', alpha=.5)
     plt.ylabel('Fitness Scores')
     plt.xlabel('Generations')
-    plt.axis([0,g.Gen, 0, 1+.5*max(max_fitness)])
+    plt.axis([0,g.generation, 0, 1+.5*max(max_fitness)])
     plt.grid(visible=True, which='major', color = '#666666', linestyle = '-', linewidth =0.5)
     plt.minorticks_on()
     plt.grid(visible=True, which = 'minor', color = '#999999', linestyle = '-', linewidth=0.2, alpha = 0.5)
@@ -55,9 +55,10 @@ for z in range(0, g.Gen+1):
     fitness_scores.clear()
 
 # plot max and Ave lines
-generations = [f for f in range(0, g.Gen+1)]
+generations = [f for f in range(0, g.generation+1)]
 plt.plot(generations, max_fitness, linestyle = '-', color = 'blue', alpha = 0.5)
 plt.plot(generations, ave_fitness, linestyle = '--', color = 'red', alpha = 0.5)
+plt.legend()
 plt.savefig("fitness.png")
 
 
@@ -78,14 +79,14 @@ for z in range(0, g.Gen+1):
     ave_chi.append(mean(chi_scores))
     
     #populate generation vector for plotting
-    generations = [z for f in range(0, g.Npop)]
+    generations = [z for f in range(0, g.population)]
     
     # begin plotting
-    plt.title(g.Design + 'Chi-Squared Evolution test')
+    plt.title(g.design + ' Chi-Squared Evolution test')
     plt.plot(generations, chi_scores, '.', color = 'black', markersize = '5.0', alpha=.5)
     plt.ylabel('Chi-Squared')
     plt.xlabel('Generations')
-    plt.axis([0,g.Gen, 0, 1])
+    plt.axis([0,g.generation, 0, 1])
     plt.grid(visible=True, which='major', color = '#666666', linestyle = '-', linewidth =0.5)
     plt.minorticks_on()
     plt.grid(visible=True, which = 'minor', color = '#999999', linestyle = '-', linewidth=0.2, alpha = 0.5)
@@ -93,9 +94,10 @@ for z in range(0, g.Gen+1):
     chi_scores.clear()
     
 # plot min and Ave lines
-generations = [f for f in range(0, g.Gen+1)]
+generations = [f for f in range(0, g.generation+1)]
 plt.plot(generations, min_chi, linestyle = '-', color = 'blue', alpha = 0.5)
 plt.plot(generations, ave_chi, linestyle = '--', color = 'red', alpha = 0.5)
+plt.legend()
 plt.savefig("chisquared.png")
 
 # Display final generation thresholds
