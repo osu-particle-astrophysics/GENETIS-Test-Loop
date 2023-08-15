5bf7627d701112f4be6223b30b33ac5a81f92a51
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-08-14 20:36:19 -0400

    cleanup in progress

---
+++ file_cleanup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+## Created by Ryan T Debolt
+## created on 10/23/2020
+## Created for: GENETIS Research group at Ohio State University
+
+# set directories/paths
+PlotsPath='Plots'
+RunPath='Run'
+GAPath='GA/SourceFiles'
+
+# Compile the GA in its directory
+g++ -std=c++11 $GAPath/New_GA.cpp -o $GAPath/GA.exe
+
+# Set Constants
+design="ARA"
+generations=50
+population=100
+
+# Initialize job submission variables
+count=0
+start=0
+
+# Loop over variables: define them in their ranges in their loops
+for rank in {20..100..10} 
+do
+    for roulette in {0..100..10} 
+    do
+        for tournament in {0..100..10} 
+        do
+            selection=$(( $rank + $roulette + $tournament ))
+            if [ $selection -eq $population ]
+            then  
+                for reproduction in {4..16..4} 
+                do
+                    for crossover in {72..100..4} 
+                    do
+                        for mutation in {8..20..4}
+                        do
+                            opperators=$(( $reproduction + $crossover + $mutation ))
+                            if [ $opperators -le $population ]
+                            then
+                                for sigma in {10..10..5} 
+                                do
+                                    for test in {1..10..1}
+                                    do
+                                        # Submit run
+                                        sbatch test_run.sh ${design} ${generations} ${population} ${rank} ${roulette} ${tournament} ${reproduction} ${crossover} ${mutation} ${sigma} ${test}
+                                        count=$((count+1))
+                                        if [ $count -ge 250 ]
+                                        then
+                                            echo batch submitted
+                                            nfiles=$(ls $PlotsPath/ | wc -l)
+                                            while [[ $(((nfiles)%500)) -ne 0 || $nfiles -eq $start ]] 
+                                            do
+                                                nfiles=$(ls $PlotsPath/ | wc -l)
+                                                echo $nfiles
+                                                sleep 10
+                                            done
+                                            start=$nfiles
+                                            count=0
+                                        fi
+                                    done
+                                done
+                            fi
+                        done
+                    done
+                done
+            fi
+        done
+    done
+done
+
--- test_write.py
+++ test_write.py
@@ -6,10 +6,10 @@ import argparse
 
 
 # Functions
-def read_data(generation):
+def read_data(generation, runname):
     """Read and return data from file."""
     data = []
-    with open(f'{generation}_generationData.csv', 'r') as file:
+    with open(f'{runname}_{generation}_generationData.csv', 'r') as file:
         file_lines = file.readlines()[4:]
 
     for line in file_lines:
@@ -24,6 +24,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("design", type=str)
 parser.add_argument("generations", type=int)
 parser.add_argument("population", type=int)
+parser.add_argument("runname", type=str)
 arg = parser.parse_args()
 
 # Find DNA vector parameters based on design
@@ -42,11 +43,11 @@ elif arg.design == "PUEO":
 # Read data from generationData.csv into lines array
 lines = []
 for gen in range(0, arg.generations+1):
-    lines.append(read_data(gen))
+    lines.append(read_data(gen, arg.runname))
 
 
 # Print data to file
-with open('testData.csv', "w") as file:
+with open(f'{arg.runname}_testData.csv', "w") as file:
     # Write out the header
     file.write("Test Data for \n")
     file.write("Generation, Individual, Metric, Fitness, Error, Parent 1,")
