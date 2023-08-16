0d8d8ee14c31348e218146dbd714d67395808ac9
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-08-16 16:19:52 -0400

    prepping for master files

--- master_write.py
+++ master_write.py
@@ -2,28 +2,54 @@
 # by: Ryan T Debolt
 """Go through each file, gather and plot/save important information."""
 import csv
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import cm
-import matplotlib.colors as mcolors
-from cycler import cycler
 import statistics as stat
 from statistics import mean
 import numpy as np
 
 design = "ARA"
 population = 100
-error = "0.25"
+generations = 50
+error = "0.0"
 
 count = 0
+run_min_metric = []
 earliest_gen = []
 average_gen = []
-average_gen2 = []
 sigma_average_gen = []
-sigma_average_gen2 = []
-run_name = []
-bench_mark = 0.1
-bench_mark2 = 0.05
+runtype = []
+benchmark = 0.1
 
+
+def read_data(runname):
+    """Read in data from csv."""
+    # Set arrays
+    gen = []
+    metric = []
+    subgen = []
+    submetric = []
+
+    # Open files
+    with open(f"{runname}_testData.csv") as file:
+        csv_read = csv.reader(file, delimiter=',')
+        for i, row in enumerate(csv_read):
+            if i > 1:
+                if (len(subgen) < population):
+                    subgen.append(float(row[0]))
+                    submetric.append(float(row[2]))
+                if (len(subgen) == population):
+                    gen.append(subgen)
+                    subgen.clear()
+                    metric.appens(submetric)
+                    submetric.clear()
+    return gen, metric
+
+
+# MAIN
+
+print("Starting")
+
+print("Gathering Run Names")
+# Make list of runnames
 for rank in range(60, 61, 1):
     for roulette in range(20, 21, 1):
         for tournament in range(20, 21, 1):
@@ -35,70 +61,33 @@ for rank in range(60, 61, 1):
                             operators = crossover + reproduction + mutation
                             if operators <= population:
                                 for sigma in range(5, 6, 5):
-                                    count = count+1
-                                    color = iter(cm.rainbow(np.linspace(0, 1, 11)))
-                                    temp_benchmark_gen = []
-                                    temp_benchmark_gen2 = []
-                                    run_name.append(f"{rank}_{roulette}_{tournament}_{reproduction}_{crossover}_{mutation}_{sigma}")
-                                    for test in range(1, 101):
-                                        temp_earliest = 50
-                                        temp_earliest2 = 50
-                                        c = 'blue' #next(color)
-                                        ave_fitness = []
-                                        max_fitness = []
-                                        ave_metric = []
-                                        min_metric = []
-                                        gen_num = []
-                                        for generation in range(0, 51, 1):
-                                            gen_num.append(generation)
-                                        for gens in range(0, 51):
-                                            runname = f"{rank}_{roulette}_{tournament}_{reproduction}_{crossover}_{mutation}_{sigma}_{test}_{gens}"
-                                            fit = []
-                                            metric = []
-                                            with open(f"{runname}_generationData.csv") as F1:
-                                                txt_read = csv.reader(F1, delimiter=",")
-                                                for i, row in enumerate(txt_read):
-                                                    if i > 3:
-                                                        fit.append(float(row[2]))
-                                                        metric.append(float(row[1]))
-                                            ave_fitness.append(mean(fit))
-                                            max_fitness.append(max(fit))
-                                            ave_metric.append(mean(metric))
-                                            min_metric.append(min(metric))
+                                    runtype.append(f"{rank}_{roulette}_{tournament}_{reproduction}_{crossover}_{mutation}_{sigma}")
 
-                                            # Now update temp_earliest
-                                            if min(metric) <= bench_mark and gens < temp_earliest:
-                                                temp_earliest = gens
-                                            if min(metric) <= bench_mark2 and gens < temp_earliest2:
-                                                temp_earliest2 = gens
-                                        temp_benchmark_gen.append(temp_earliest)
-                                        temp_benchmark_gen2.append(temp_earliest2)
+print("Analyzing Data")
+# For each runtype, test gather information
+for run in range(len(runtype)):
+    min_metric = 1.0
+    earliest = []
+    for test in range(1, 11):
+        runname = f"{runtype[run]}_{test}"
+        min_gen = 50
+        gen, metric = read_data(runname)
+        for g in range(0, generations+1):
+            if (min(metric[g]) < min_metric):
+                min_metric = min(metric[g])
+            if (min(metric[g]) <= benchmark and g <= min_gen):
+                earliest.append(g)
+    earliest_gen.append(min(earliest))
+    average_gen.append(mean(earliest))
+    sigma_average_gen.append(stat.pstdev(earliest))
+    earliest.clear()
 
-                                        # Begin Plotting
-                                        plt.figure(count, figsize=(16,9))
-                                        plt.plot(gen_num, ave_metric, c=c, linestyle='dotted', label=(f"Run {test} average"), alpha=0.1)
-                                        plt.plot(gen_num, min_metric, c=c, linestyle='solid', label=(f"Run {test} High"), alpha=0.1)
-                                        # plt.axis([0,50, 0.00, 1.5])
-                                        plt.grid(visible=True, which='major', color='#666666', linestyle='-', linewidth=0.5)
-                                        plt.minorticks_on()
-                                        plt.grid(visible=True, which='minor', color='#999999', linestyle='-', linewidth=0.2, alpha=0.5)
-                                        plt.ylabel('Fitness Score')
-                                        plt.xlabel('Generations')
-                                        plt.suptitle(f"{design} Population: {population} Error: {error}")
-                                        plt.savefig(f"plot_{design}_{error}_{rank}_{roulette}_{tournament}_{reproduction}_{crossover}_{mutation}_{sigma}.png")
-                                    plt.close()
-                                    print(f"plot_{design}_{error}_{rank}_{roulette}_{tournament}_{reproduction}_{crossover}_{mutation}_{sigma}.png written")
-                                    earliest_gen.append(min(temp_benchmark_gen))
-                                    average_gen.append(mean(temp_benchmark_gen))
-                                    average_gen2.append(mean(temp_benchmark_gen2))
-                                    sigma_average_gen.append(stat.pstdev(temp_benchmark_gen))
-                                    sigma_average_gen2.append(stat.pstdev(temp_benchmark_gen2))
-
-
+print("Creating Master File")
+# Print data into master file
 with open("Master_gen.csv", 'w') as f:
-    f.write("Run Name, \t Earliest, \t Average (.5), \t Standard Deviation (.5), \t Average (.25), \t Standard Deviation (.25) \n")
-    for x in range(0, len(run_name)):
-        f.write(f"{run_name[x]}, {earliest_gen[x]}, {average_gen[x]}, {sigma_average_gen[x]}, {average_gen2[x]}, {sigma_average_gen2[x]}\n")
+    f.write("Run Name, Earliest Generation, Average Generations, Standard Deviation , Minimum Metric \n")
+    for x in range(0, len(runname)):
+        f.write(f"{runtype[x]}, {earliest_gen[x]}, {average_gen[x]}, {sigma_average_gen[x]}, {min_metric[x]} \n")
 
 f.close()
 print("Master_gen.csv written")
