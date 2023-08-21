e43606b50fe5013f71c45644e9cde27d693b88a8
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-08-21 16:43:00 -0400

    Update test_write.py

--- test_write.py
+++ test_write.py
@@ -11,10 +11,11 @@ def read_data(generation, runname):
     data = []
     with open(f'{runname}_{generation}_generationData.csv', 'r') as file:
         file_lines = file.readlines()[4:]
+        seed = file.readlines()[1]
 
     for line in file_lines:
         data.append(line)
-    return data
+    return data, seed
 
 
 # Main
@@ -42,21 +43,25 @@ elif arg.design == "PUEO":
 
 # Read data from generationData.csv into lines array
 lines = []
+seeds = []
 for gen in range(0, arg.generations+1):
-    lines.append(read_data(gen, arg.runname))
+    data, seed = read_data(gen, arg.runname)
+    lines.append(data.copy)
+    seeds.append(seed)
+    data.clear()
 
 
 # Print data to file
 with open(f'{arg.runname}_testData.csv', "w") as file:
     # Write out the header
     file.write("Test Data for \n")
-    file.write("Generation, Individual, Metric, Fitness, Error, Parent 1,")
+    file.write("Generation, Seed, Individual, Metric, Fitness, Error, Parent 1,")
     file.write(" Parent 2, Opperator")
     for x in range(sections*genes):
         file.write(f", Gene {x+1}")
     file.write('\n')
     for gen in range(0, arg.generations+1):
         for pop in range(0, arg.population):
-            file.write(f'{gen}, {lines[gen][pop]}')
+            file.write(f'{gen}, {seeds[gen]}, {lines[gen][pop]}')
 
 print("testData Written")
