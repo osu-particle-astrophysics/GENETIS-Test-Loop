3460fe8afb5a357872ef2a1f855455a15a48a97f
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-09-04 22:33:52 -0400

    Update single_run.sh

--- single_run.sh
+++ single_run.sh
@@ -16,14 +16,14 @@ GAPath='/users/PAS0654/ryantdebolt/test_loop_build_directory/GA/SourceFiles'
 design="ARA"
 generations=50
 population=100
-rank=50
-roulette=40
-tournament=10
-reproduction=4
-crossover=76
-mutation_no=20
+rank=80
+roulette=0
+tournament=20
+reproduction=12
+crossover=72
+mutation_no=12
 sigma=10
-test=6
+test=8
 
 # establish run name
 runname=${rank}'_'${roulette}'_'${tournament}'_'${reproduction}'_'${crossover}'_'${mutation_no}'_'${sigma}'_'${test}
