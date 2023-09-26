54188e4ba9016a5c6c8af9426be6bb7912921cd3
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-09-25 20:11:24 -0400

    Update test_loop.sh

--- test_loop.sh
+++ test_loop.sh
@@ -22,27 +22,27 @@ count=0
 start=0
 
 # Loop over variables: define them in their ranges in their loops
-for rank in {0..50..5} 
+for rank in {20..20..5} 
 do
-    for roulette in {0..50..5} 
+    for roulette in {0..0..5} 
     do
-        for tournament in {0..50..5} 
+        for tournament in {30..30..5} 
         do
             selection=$(( $rank + $roulette + $tournament ))
             if [ $selection -eq $population ]
             then  
-                for reproduction in {2..8..2} 
+                for reproduction in {6..6..2} 
                 do
-                    for crossover in {36..45..2} 
+                    for crossover in {36..36..2} 
                     do
-                        for mutation in {4..10..2}
+                        for mutation in {4..4..2}
                         do
                             opperators=$(( $reproduction + $crossover + $mutation ))
                             if [ $opperators -le $population ]
                             then
                                 for sigma in {10..10..5} 
                                 do
-                                    for test in {1..10..1}
+                                    for test in {1..100..1}
                                     do
                                         # Submit run
                                         sbatch test_run.sh ${design} ${generations} ${population} ${rank} ${roulette} ${tournament} ${reproduction} ${crossover} ${mutation} ${sigma} ${test}
